1f8a73c6c266a16edf3efb3f59431c726304aecc
TakahiroMashiko <takahiromashiko3122@gmail.com>
2021-03-28 10:20:24 +0000

    [Update] 3-8 bookの新規登録機能の作成]

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -10,8 +10,17 @@ class BooksController < ApplicationController
   end
 
   def create
+    book = Book.new(book_params)
+    book.save
+    redirect_to books_path
+
   end
 
   def edit
   end
-end
+
+# ストロングパラメータ
+  private
+  def book_params
+    params.require(:book).permit(:title, :body)
+  end
