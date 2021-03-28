c1012b3ffecaf09ae904c51c4153ff89ff8b9414
TakahiroMashiko <takahiromashiko3122@gmail.com>
2021-03-28 07:16:34 +0000

    [Add] 3-8 ルーティングの記述変更

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,5 @@
 Rails.application.routes.draw do
-  get 'lists/index'
-  get 'lists/show'
-  get 'lists/new'
-  get 'lists/edit'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :homes
+  resources :books
 end
