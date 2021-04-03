6191588d48ab0874df8d86ec7095ebf462642b42
TakahiroMashiko <takahiromashiko3122@gmail.com>
2021-04-03 03:46:00 +0000

    [Finish] 3-8 bookのindexページの作成

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -6,6 +6,7 @@ class BooksController < ApplicationController
 
   def index
     @books = Book.all
+    @book = Book.new
   end
 
   def create
@@ -22,12 +23,11 @@ class BooksController < ApplicationController
     @book = Book.find(params[:id])
   end
 
-  # def update
-    # book = Book.find(params[:id])
-    # book.update(book_params)
-    # redirect_to book_path(book.id)
-
-  # end
+  def update
+    book = Book.find(params[:id])
+    book.update(book_params)
+    redirect_to book_path(book.id)
+  end
 
   def destroy
     book = Book.find(params[:id])
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :homes
   resources :books
+  delete 'books/:id' => 'books#destroy', as: 'destroy_book'
 end
