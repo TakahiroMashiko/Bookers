7d18271099e882c1ed17735baabd847b04c5c994
TakahiroMashiko <takahiromashiko3122@gmail.com>
2021-04-03 07:04:32 +0000

    [Update] 3-8 bookのeditページの作成

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :homes
   resources :books
+  get 'books/:id/edit' => 'books#edit'
   delete 'books/:id' => 'books#destroy', as: 'destroy_book'
 end
